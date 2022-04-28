6fab1346e3fc2be1596324d9f9d4d376a48b65f2
0x00 <101659928+0x00Nu11@users.noreply.github.com>
2022-04-29 01:23:06 +0300

    updates
    - changed repo name
    - added 2 more engines: duckduckgo and yandex
    - updated README.md with how to

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# custom-searx-homepage
-![preview](preview.png)
-simple custom searx homepage for chromium (or any browser really) because ungoogled chromium doesn't have any way I know of to customize homepage and new tabs
+# custom-homepage
+![preview-searx](.assets/preview-searx.png)
+![preview-duckduckgo](.assets/preview-duckduckgo.png)
+![preview-yandex](.assets/preview-yandex.png)
+## what's this
+simple custom homepage for chromium (or any browser really), because ungoogled chromium doesn't have any way I know of to customize homepage and new tabs.
+## how to:
+1. download the zip or clone the repo
+2. extract the zip into a directory unlikely to change, or if cloned make sure it's cloned in a directory unlikely to change
+3. 
+ - if on chromium based browser:
+	1. go to `chrome://flags/`
+	2. look for the "Custom New Tab Page" flag
+	3. enable, and set the path to your_directory/any_html.html, e.g: `/home/0x00Nu11/customHomepage/searx.html`
+- if on gecko based browser:
+	1. versions 41+ no longer support the browser.newtab.url setting in `about:config`, so you have to rely on an extension. I suggest [New Tab Override by Sören Hentzschel
+](https://addons.mozilla.org/en-US/firefox/addon/new-tab-homepage/)
+	2. set the new tab either to `about:home` or to your_directory/any_html.html, e.g: `/home/0x00Nu11/customHomepage/searx.html`
+	3. go to `about:preferences#home` and set the homepage and new windows to your_directory/any_html.html, e.g: `/home/0x00Nu11/customHomepage/searx.html`
--- script.js
+++ script.js
@@ -1,6 +1,79 @@
-searchWrapper=document.getElementById('search-wrapper');
-searchBar=document.getElementById('search-bar');
-searxIcon=document.getElementById('searx-icon');
+var searchWrapper=document.getElementById('search-wrapper');
+var searchIcon=document.getElementById('search-icon');
+var searchBar=document.getElementById('search-bar');
+var searxEngineImage=document.getElementById('searx-engine-image');
+var duckduckgoEngineImage=document.getElementById('duckduckgo-engine-image');
+var yandexEngineImage=document.getElementById('yandex-engine-image');
+var tabTitle=document.getElementById('title').innerText;
+
+function searchWith(searchEngine, searchQuery) {
+    if (searchEngine=='searx') {
+        return 'https://swag.pw/search?q='+searchQuery;
+    } else if (searchEngine=='duckduckgo') {
+        return 'https://duckduckgo.com/?q='+searchQuery;
+    } else {
+        return 'https://yandex.com/images/search?text='+searchQuery;
+    }
+}
+
+if (tabTitle=='New Searx Tab') {
+    searchBar.addEventListener('mouseover', function() {
+        searchBar.placeholder='Type something...';
+        searchBar.focus();
+    })
+    searchBar.addEventListener('mouseout', function() {
+        searchBar.placeholder='Meta search';
+    })
+    searchBar.addEventListener('keypress', function(e) {
+        if (e.key==='Enter') {
+            doSearch('searx');
+        }
+    })
+    searchIcon.addEventListener('mouseover', function() {
+        searchIcon.src='img/searx-icon-flipped.png';
+    })
+    searchIcon.addEventListener('mouseout', function(){
+        searchIcon.src='img/searx-icon.png';
+    })
+} else if (tabTitle=='New DuckDuckGo Tab') {
+    searchBar.addEventListener('mouseover', function() {
+        searchBar.placeholder='Type something...';
+        searchBar.focus();
+    })
+    searchBar.addEventListener('mouseout', function() {
+        searchBar.placeholder='Private search';
+    })
+    searchBar.addEventListener('keypress', function(e) {
+        if (e.key==='Enter') {
+            doSearch('duckduckgo');
+        }
+    })
+    searchIcon.addEventListener('mouseover', function() {
+        searchIcon.src='img/duckduckgo-icon-flipped.png';
+    })
+    searchIcon.addEventListener('mouseout', function(){
+        searchIcon.src='img/duckduckgo-icon.png';
+    })
+} else {
+    searchBar.addEventListener('mouseover', function() {
+        searchBar.placeholder='Type something...';
+        searchBar.focus();
+    })
+    searchBar.addEventListener('mouseout', function() {
+        searchBar.placeholder='Image search';
+    })
+    searchBar.addEventListener('keypress', function(e) {
+        if (e.key==='Enter') {
+            doSearch('yandex');
+        }
+    })
+    searchIcon.addEventListener('mouseover', function() {
+        searchIcon.src='img/yandex-icon-flipped.png';
+    })
+    searchIcon.addEventListener('mouseout', function(){
+        searchIcon.src='img/yandex-icon.png';
+    })
+}
 
 searchWrapper.addEventListener('mouseover', function() {
     searchWrapper.style.boxShadow='0 0.25rem 1rem rgba(255, 255, 255, 0.589)';
@@ -9,30 +82,32 @@ searchWrapper.addEventListener('mouseout', function() {
     searchWrapper.style.boxShadow='0 0 0 0';
 })
 
-searxIcon.addEventListener('mouseover', function() {
-    searxIcon.src='img/searx-icon-flipped.png';
+searxEngineImage.addEventListener('mouseover', function() {
+    searxEngineImage.style.boxShadow='0 0.25rem 1rem rgba(255, 255, 255, 0.589)';
+    searxEngineImage.src='img/searx-icon-flipped.png'
 })
-searxIcon.addEventListener('mouseout', function() {
-    searxIcon.src='img/searx-icon.png';
+searxEngineImage.addEventListener('mouseout', function() {
+    searxEngineImage.style.boxShadow='0 0 0 0';
+    searxEngineImage.src='img/searx-icon.png'
+})
+duckduckgoEngineImage.addEventListener('mouseover', function() {
+    duckduckgoEngineImage.style.boxShadow='0 0.25rem 1rem rgba(255, 255, 255, 0.589)';
+    duckduckgoEngineImage.src='img/duckduckgo-icon-flipped.png'
+})
+duckduckgoEngineImage.addEventListener('mouseout', function() {
+    duckduckgoEngineImage.style.boxShadow='0 0 0 0';
+    duckduckgoEngineImage.src='img/duckduckgo-icon.png'
+})
+yandexEngineImage.addEventListener('mouseover', function() {
+    yandexEngineImage.style.boxShadow='0 0.25rem 1rem rgba(255, 255, 255, 0.589)';
+    yandexEngineImage.src='img/yandex-icon-flipped.png'
+})
+yandexEngineImage.addEventListener('mouseout', function() {
+    yandexEngineImage.style.boxShadow='0 0 0 0';
+    yandexEngineImage.src='img/yandex-icon.png'
 })
 
-searchBar.addEventListener('mouseover', function() {
-    searchBar.placeholder='Search...';
-    searchBar.focus();
-})
-searchBar.addEventListener('mouseout', function() {
-    searchBar.placeholder='Search';
-})
-searchBar.addEventListener('keypress', function(e) {
-    if (e.key==='Enter'){
-        doSearch();
-    }
-})
-
-function doSearch() {
-    let searchURL='https://swag.pw/search?q='+searchBar.value
-    +'&language=en-US&time_range=None&safesearch=0&categories=general';
+function doSearch(searchEngine) {
+    let searchURL=searchWith(searchEngine, searchBar.value);
     window.open(searchURL, '_self');
-    searxIcon.src='img/searx-icon-flipped.png';
-    searchBar.value='';
 }
